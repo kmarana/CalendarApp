7da200172470e10e86bb34c308eb327cf328dd8b
kmarana <105758978+kmarana@users.noreply.github.com>
2022-07-13 16:55:42 -0400

    Add files via upload
    Co-authored-by: juliakemp <no-reply@email.com>

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+//import Cards from '../Cards';
+import HeroSection from '../components/HeroSection';
+//import Footer from '../Footer';
+import Login from '../components/Login';
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection />
+    </>
+  );
+}
+
+export default Home;
