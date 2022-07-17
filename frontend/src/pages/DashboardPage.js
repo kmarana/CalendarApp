d5da839eb0d4349a03b9f3581c0d6414db55b315
kmarana <105758978+kmarana@users.noreply.github.com>
2022-07-16 22:13:43 -0400

    Add files via upload

---
+++ frontend/src/pages/DashboardPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import HeroSection from '../components/HeroSection';
+
+
+function DashboardPage() {
+    return (
+        <body>
+            Welcome, ___
+    </body>
+    );
+  }
+
+  export default DashboardPage;
