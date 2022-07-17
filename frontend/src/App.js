c5315c93406579df6c2f330865f0ad98932f72b1
kmarana <105758978+kmarana@users.noreply.github.com>
2022-07-16 22:12:52 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+//import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import LoginPage from './pages/LoginPage';
 import CardPage from './pages/CardPage';
+import RegisterPage from './pages/RegisterPage';
+import EmailVerifPage from './pages/EmailVerifPage';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" index element={<LoginPage />} />
-      <Route path="/cards" index element={<CardPage />} />
-    </Routes>
-  </BrowserRouter>
+    <>
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" index element={<Home />} />   
+        <Route path="/login" index element={<LoginPage />} /> 
+        <Route path="/cards" index element={<CardPage />} />  
+        <Route path="/register" index element={<RegisterPage />} />  
+        <Route path="/emailVerif" index element={<EmailVerifPage />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
